493fa9b859c95a5acc5a6a0ee537559b0d180bba
An <antran1245@gmail.com>
2025-06-29 14:21:09 -0700

    adding some info on LabelRadio component

--- src/components/ui/LabelRadio.tsx
+++ src/components/ui/LabelRadio.tsx
@@ -1,6 +1,18 @@
+/**
+ * Label container with Input radio component
+ *
+ * A reuseable UI label+radio component.
+ *
+ * Usage:
+ * <LabelRadio label={"click me"}/>
+ */
+
 import React from "react";
 
 interface LabelRadioProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  /**
+   * Label the radio
+   */
   label: string;
 }
 
