8440e79ce08c41a75a9f8736a0fac0fa1d5130ea
An <antran1245@gmail.com>
2025-07-16 21:37:40 -0700

    added detail to view on selecting a saved session

--- src/features/SavedSessionList/SessionItemContent.tsx
+++ src/features/SavedSessionList/SessionItemContent.tsx
@@ -1,12 +1,48 @@
-import React from "react";
-import type { SessionType } from "@shared/chrome/storage";
+import React, { useEffect, useState } from "react";
+import type { BrowserType, SessionType } from "@shared/chrome/storage";
 
 interface SessionItemContentProps {
   session: SessionType;
 }
 
 const SessionItemContent: React.FC<SessionItemContentProps> = ({ session }) => {
-  return <div>{session && <p>{session.name}</p>}</div>;
+  const [formatSessions, setFormatSessions] = useState<BrowserType>({});
+
+  useEffect(() => {
+    let updateSession: BrowserType = {};
+    for (const browser in session["browsers"]) {
+      let activeTabTitle = "";
+      const tabList = session["browsers"][browser];
+      console.log(tabList, session["browsers"], browser);
+      for (const tab of tabList) {
+        if (tab.active) {
+          activeTabTitle = tab.title;
+          break;
+        }
+      }
+      if (activeTabTitle !== "") {
+        updateSession[activeTabTitle] = tabList;
+      } else {
+        updateSession[browser] = tabList;
+      }
+    }
+    setFormatSessions(updateSession);
+  }, [session]);
+  return (
+    <div>
+      <p>{session.name}</p>
+      {Object.entries(formatSessions).map(([title, tabs]) => (
+        <div>
+          <p>{title}</p>
+          <div>
+            {Object.values(tabs).map((key) => (
+              <div>{key.title}</div>
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default SessionItemContent;
