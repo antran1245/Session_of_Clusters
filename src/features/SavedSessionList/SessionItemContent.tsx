43d928074f814bb26f10302645b06ae8ba5142e9
An <antran1245@gmail.com>
2025-07-19 10:39:11 -0700

    comments on session item content

--- src/features/SavedSessionList/SessionItemContent.tsx
+++ src/features/SavedSessionList/SessionItemContent.tsx
@@ -19,22 +19,35 @@ const SessionItemContent: React.FC<SessionItemContentProps> = ({ session }) => {
     [id: string]: string;
   }>({});
 
+  /**
+   * Delete a tab from a selected browser/window within a selected Session.
+   * @param urlData - Data to update
+   * @param browserID - Browser key to update
+   */
   function deleteTab(urlData: URLType, browserID: string) {
     let updateSession = session.browsers[browserID];
+    // Index to remove
     const indexToRemove = updateSession.findIndex(
       (obj) => obj.url === urlData.url
     );
+    // If exist then remove the selected data
     if (indexToRemove !== -1) updateSession.splice(indexToRemove, 1);
     session.browsers[browserID] = updateSession;
-    if (session.browsers[browserID].length === 0) {
+    // If browser/window is empty after deleting, then remove the window/browser.
+    if (session.browsers[browserID].length === 0)
       delete session.browsers[browserID];
-    }
+
+    // Update the state and cause re-render.
     setSelectedSession({ ...sessions[selectedSession.name] });
     updateStorageSession({ ...sessions });
   }
 
+  /**
+   * On change to selectedSession, an object will be created to show the title of active for each browser.
+   */
   useEffect(() => {
     let filterTitleBrowser: { [id: string]: string } = {};
+    // Filter session for active tab for each browser.
     for (const browser in session["browsers"]) {
       let activeTabTitle = "";
       const tabList = session["browsers"][browser];
@@ -56,9 +69,19 @@ const SessionItemContent: React.FC<SessionItemContentProps> = ({ session }) => {
   return (
     <div className="text-left flex flex-col gap-1">
       <p className="font-extrabold text-[#93DEFF]">{selectedSession.name}</p>
-      {Object.entries(selectedSession.browsers).map(([title, tabs]) => (
-        <Accordion title={activeTabs[title]}>
-          {Object.values(tabs).map((key, index) => (
+      {/**
+       * Iterate through session browers.
+       * id: Browser ID
+       * tabs: Array of url objects
+       */}
+      {Object.entries(selectedSession.browsers).map(([id, tabs]) => (
+        <Accordion title={activeTabs[id]}>
+          {/**
+           * Iterate through each tab object
+           * key: object key
+           * index: array index
+           */}
+          {tabs.map((key, index) => (
             <div
               className={`grid grid-cols-12 gap-1 items-center border-b border-x border-[#F7F7F7] p-2 ${
                 index === Object.values(tabs).length - 1 ? "rounded-b" : ""
@@ -69,7 +92,7 @@ const SessionItemContent: React.FC<SessionItemContentProps> = ({ session }) => {
               </a>
               <Button
                 onClick={() => {
-                  deleteTab(key, title);
+                  deleteTab(key, id);
                 }}
                 className="w-fit h-fit p-1.5 col-span-1"
               >
